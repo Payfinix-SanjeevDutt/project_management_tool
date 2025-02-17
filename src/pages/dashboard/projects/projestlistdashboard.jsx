213056533e4050c9acdb528fbf671cefbd835039
Niveditha8970 <nniveditha8970@gmail.com>
2025-02-17 15:17:17 +0530

    project_list dashboard

---
+++ src/pages/dashboard/projects/projestlistdashboard.jsx
@@ -0,0 +1,14 @@
+import ProjectDashboardView from 'src/sections/project/project-dashboard/project-dashboard-view';
+
+
+// ----------------------------------------------------------------------
+
+
+export default function ProjectListDashboard() {
+    return (
+        // <ProjectDashboardView />
+        <>
+        <h1>List dashboard</h1>
+        </>
+    );
+}
